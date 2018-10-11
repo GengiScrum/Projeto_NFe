2afeb6df42aaec870e30d49fa9e184b9b9cfa4cb
Leticia <leticiaventuradossantos@gmail.com>
2018-10-11 17:43:28 -0300

    corrigido registerCommand

--- Projeto_NFe/Client/src/app/features/shipping-company/shared/shipping-company.model.ts
+++ Projeto_NFe/Client/src/app/features/shipping-company/shared/shipping-company.model.ts
@@ -1,4 +1,5 @@
 import { Address } from '../../address/address.model';
+import { PersonType } from './person-type.enum';
 
 export class ShippingCompany {
     public id: number;
@@ -21,7 +22,7 @@ export class ShippingCompanyRegisterCommand {
     public corporateName: string;
     public cnpj: string;
     public cpf: string;
-    public personType: number;
+    public personType: string;
     public stateRegistration: string;
     public streetName: string;
     public number: number;
@@ -30,19 +31,22 @@ export class ShippingCompanyRegisterCommand {
     public state: string;
     public country: string;
 
-    constructor(shippingCompany: ShippingCompany, address: Address) {
-        this.businessName = shippingCompany.businessName;
-        this.corporateName = shippingCompany.corporateName;
-        this.cnpj = shippingCompany.cnpj;
-        this.cpf = shippingCompany.cpf;
-        this.stateRegistration = shippingCompany.stateRegistration;
-        this.personType = shippingCompany.personType;
-        this.streetName = address.streetName;
-        this.number = address.number;
-        this.neighborhood = address.neighborhood;
-        this.city = address.city;
-        this.state = address.state;
-        this.country = address.country;
+    constructor(formModel: any) {
+        this.businessName = formModel.businessName;
+        this.personType = formModel.personType;
+        this.streetName = formModel.address.streetName;
+        this.number = formModel.address.number;
+        this.neighborhood = formModel.address.neighborhood;
+        this.city = formModel.address.city;
+        this.state = formModel.address.state;
+        this.country = formModel.address.country;
+        if (this.personType === PersonType.PERSON) {
+            this.cpf = formModel.person.cpf;
+        } else {
+            this.cnpj = formModel.enterprise.cnpj;
+            this.stateRegistration = formModel.enterprise.stateRegistration;
+            this.corporateName = formModel.enterprise.corporateName;
+        }
     }
 }
 
